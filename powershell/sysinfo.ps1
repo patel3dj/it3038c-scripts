7820b09fc8483276e196bdf61a8814672d845ec2
patel3dj <patel3dj@mail.uc.edu>
2021-09-29 02:24:14 -0400

    Initial commit

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,17 @@
 ﻿function getIP {
  (Get-NetIPaddress).IPv4address | select-string "192*" 
 }
-$IP = getIP
-write-host("This machine's IP is $IP")
-write-host("this machine's IP is {0}" -f $IP)
+function getDATE {
+Get-Date
+}
 
+$IP = getIP
+$USER = $env:USERNAME
+$HOSTT = hostname
+$PSVERSION= $PSversionTable
+$DATE = getDATE
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTT. Powershell version is $PSVERSION. Today's Date is $DATE."
+
+
+Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "dhruvj2424@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
